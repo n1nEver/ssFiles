80080ee59af966d4257eca22be1dd144dede8160
zhaoyao <393983445@qq.com>
2019-12-12 17:47:50 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 apt-get install shadowsocks-libev
+
 cd ssFiles
 cp -R shadowsocks-libev /etc
 cp -R kcptun /etc
-ln -s server-linux-amd64 /bin/server-linux-amd64
-ln -s auto-run-ss /bin/auto-run-ss
+ln -sf /root/ssFiles/server_linux_amd64 /usr/bin/server_linux_amd64
+ln -sf /root/ssFiles/auto_run_ss /usr/bin/auto_run_ss
 echo 'DONE'
